17384576e1cff91cea970476df2b96d0bb5916ed
samjkwong <samkwong@stanford.edu>
2020-02-09 23:49:06 -0800

    Update piergiaj Dataset for sampling with temporal stride of 4

--- charades_experiments/charades_dataset_full.py
+++ charades_experiments/charades_dataset_full.py
@@ -1,4 +1,4 @@
-# Original contributor: piergiaj
+# Contributor: piergiaj
 # Sampling rate modifications by Samuel Kwong
 
 import torch
@@ -29,36 +29,39 @@ def video_to_tensor(pic):
 
 
 def load_rgb_frames(image_dir, vid, start, num):
-  frames = []
-  for i in range(start, start+num):
-    img = cv2.imread(os.path.join(image_dir, vid, vid+'-'+str(i).zfill(6)+'.jpg'))[:, :, [2, 1, 0]]
-    w,h,c = img.shape
-    if w < 226 or h < 226:
-        d = 226.-min(w,h)
-        sc = 1+d/min(w,h)
-        img = cv2.resize(img,dsize=(0,0),fx=sc,fy=sc)
-    img = (img/255.)*2 - 1
-    frames.append(img)
-  return np.asarray(frames, dtype=np.float32)
+    frames = []
+    for i in range(start, start+num):
+        try:
+            img = cv2.imread(os.path.join(image_dir, vid, vid+'-'+str(i).zfill(6)+'.jpg'))[:, :, [2, 1, 0]]
+        except: # duplicate last frame
+            img = frames[-1]
+        w,h,c = img.shape
+        if w < 226 or h < 226:
+            d = 226.-min(w,h)
+            sc = 1+d/min(w,h)
+            img = cv2.resize(img,dsize=(0,0),fx=sc,fy=sc)
+        img = (img/255.)*2 - 1
+        frames.append(img)
+    return np.asarray(frames, dtype=np.float32)
 
 def load_flow_frames(image_dir, vid, start, num):
-  frames = []
-  for i in range(start, start+num):
-    imgx = cv2.imread(os.path.join(image_dir, vid, vid+'-'+str(i).zfill(6)+'x.jpg'), cv2.IMREAD_GRAYSCALE)
-    imgy = cv2.imread(os.path.join(image_dir, vid, vid+'-'+str(i).zfill(6)+'y.jpg'), cv2.IMREAD_GRAYSCALE)
-    
-    w,h = imgx.shape
-    if w < 224 or h < 224:
-        d = 224.-min(w,h)
-        sc = 1+d/min(w,h)
-        imgx = cv2.resize(imgx,dsize=(0,0),fx=sc,fy=sc)
-        imgy = cv2.resize(imgy,dsize=(0,0),fx=sc,fy=sc)
+    frames = []
+    for i in range(start, start+num):
+        imgx = cv2.imread(os.path.join(image_dir, vid, vid+'-'+str(i).zfill(6)+'x.jpg'), cv2.IMREAD_GRAYSCALE)
+        imgy = cv2.imread(os.path.join(image_dir, vid, vid+'-'+str(i).zfill(6)+'y.jpg'), cv2.IMREAD_GRAYSCALE)
         
-    imgx = (imgx/255.)*2 - 1
-    imgy = (imgy/255.)*2 - 1
-    img = np.asarray([imgx, imgy]).transpose([1,2,0])
-    frames.append(img)
-  return np.asarray(frames, dtype=np.float32)
+        w,h = imgx.shape
+        if w < 224 or h < 224:
+            d = 224.-min(w,h)
+            sc = 1+d/min(w,h)
+            imgx = cv2.resize(imgx,dsize=(0,0),fx=sc,fy=sc)
+            imgy = cv2.resize(imgy,dsize=(0,0),fx=sc,fy=sc)
+             
+        imgx = (imgx/255.)*2 - 1
+        imgy = (imgy/255.)*2 - 1
+        img = np.asarray([imgx, imgy]).transpose([1,2,0])
+        frames.append(img)
+    return np.asarray(frames, dtype=np.float32)
 
 
 def make_dataset(split_file, split, root, mode, num_classes=157):
@@ -66,28 +69,36 @@ def make_dataset(split_file, split, root, mode, num_classes=157):
     with open(split_file, 'r') as f:
         data = json.load(f)
 
-    i = 0
-    for vid in data.keys():
+    for i, vid in enumerate(data.keys()):
         if (i % 500 == 0):
             print('{}/{}'.format(i, len(data.keys())))
+
         if data[vid]['subset'] != split:
             continue
         
         if not os.path.exists(os.path.join(root, vid)):
             continue
-        num_frames = len(os.listdir(os.path.join(root, vid)))
+        
+        # Sample clips with temporal stride of 4, having input clips spanning total of 125 frames (~5.2 seconds)
+        # Follows approach from Long-Term Feature Banks for Detailed Video Understanding (https://arxiv.org/pdf/1812.05038.pdf)
+        
+        num_span_frames = 125 * 4 # upper boundary to grab from original clip (duplicate last frame if clip is too short)
+        num_avail_frames = len(os.listdir(os.path.join(root, vid)))
         if mode == 'flow':
-            num_frames = num_frames//2
+            num_span_frames = num_span_frames//2
             
-        label = np.zeros((num_classes,num_frames), np.float32)
+        label = np.zeros((num_classes,num_span_frames), np.float32)
+        last_label = np.zeros((num_classes,num_span_frames), np.float32)
 
-        fps = num_frames/data[vid]['duration']
+        fps = num_avail_frames/data[vid]['duration'] # we use 24fps as provided by Charades website
         for ann in data[vid]['actions']:
-            for fr in range(0,num_frames,1):
-                if fr/fps > ann[1] and fr/fps < ann[2]:
+            for fr in range(0,num_span_frames,4): # sample every 4 frames
+                if fr < num_avail_frames:
+                    if fr/fps > ann[1] and fr/fps < ann[2]:
+                        label[ann[0], fr] = 1 # binary classification
+                else: # repeat last frame
                     label[ann[0], fr] = 1 # binary classification
-        dataset.append((vid, label, data[vid]['duration'], num_frames))
-        i += 1
+        dataset.append((vid, label, num_span_frames))
     
     return dataset
 
@@ -111,7 +122,7 @@ class Charades(data_utl.Dataset):
         Returns:
             tuple: (image, target) where target is class_index of the target class.
         """
-        vid, label, dur, nf = self.data[index]
+        vid, label, nf = self.data[index]
         if os.path.exists(os.path.join(self.save_dir, vid+'.npy')):
             return 0, 0, vid
 
